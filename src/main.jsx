43e1fc9c642748386294a0b8e0038822fcb7b978
elphoun <kale.mzhang7132@gmail.com>
2025-05-27 13:57:47 -0400

    Minor Changes to Packaging

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import './container.css'
 import App from './App'
+import { CustomerProvider, DisplayProvider, CustomerNamesProvider, TransactionProvider } from './context/Providers'
 
 createRoot(document.getElementById("root")).render(
-    <StrictMode>
-        <App />
-    </StrictMode>
+	<StrictMode>
+		<DisplayProvider>
+			<CustomerProvider>
+				<CustomerNamesProvider>
+					<TransactionProvider>
+						<App />
+					</TransactionProvider>
+				</CustomerNamesProvider>
+			</CustomerProvider>
+		</DisplayProvider>
+	</StrictMode>
 )
