b72a209d86a123da14c707ea6e4f5c186e1fa2f4
elphoun <kale.mzhang7132@gmail.com>
2025-06-02 14:04:39 -0400

    updates

--- electron/main.js
+++ electron/main.js
@@ -1,7 +1,9 @@
-import { app, BrowserWindow } from 'electron';
+import { app, BrowserWindow, ipcMain } from 'electron';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import { autoUpdater, AppUpdater } from 'electron-updater';
+import { autoUpdater } from "electron-updater";
+import electronLog from 'electron-log';
+import process from 'node:process';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -9,6 +11,10 @@ const __dirname = path.dirname(__filename);
 let mainWindow;
 autoUpdater.autoDownload = false;
 autoUpdater.autoInstallOnAppQuit = true;
+
+// Configure logging
+autoUpdater.logger = electronLog;
+autoUpdater.logger.transports.file.level = 'info';
 
 function createWindow() {
     mainWindow = new BrowserWindow({
@@ -27,15 +33,51 @@ function createWindow() {
     });
 
     mainWindow.loadFile(path.join(__dirname, '../dist/index.html'));
-    autoUpdater.checkForUpdates();
+    
+    // Check for updates
+    autoUpdater.checkForUpdatesAndNotify();
 }
+
+// Update event handlers
+autoUpdater.on('checking-for-update', () => {
+    mainWindow.webContents.send('update-status', 'Checking for updates...');
+});
+
+autoUpdater.on('update-available', () => {
+    mainWindow.webContents.send('update-status', 'Update available');
+});
+
+autoUpdater.on('update-not-available', () => {
+    mainWindow.webContents.send('update-status', 'Update not available');
+});
+
+autoUpdater.on('error', (err) => {
+    mainWindow.webContents.send('update-status', 'Error in auto-updater');
+    mainWindow.webContents.send('update-error', err);
+});
+
+autoUpdater.on('download-progress', (progressObj) => {
+    mainWindow.webContents.send('update-progress', progressObj);
+});
+
+autoUpdater.on('update-downloaded', () => {
+    mainWindow.webContents.send('update-status', 'Update downloaded');
+});
+
+// IPC handlers for update actions
+ipcMain.on('start-update', () => {
+    autoUpdater.downloadUpdate();
+});
+
+ipcMain.on('install-update', () => {
+    autoUpdater.quitAndInstall();
+});
 
 app.whenReady().then(() => {
     //additional logic here
 }).then(createWindow)
 
 app.on('window-all-closed', () => {
-    // eslint-disable-next-line no-undef
     if (process.platform !== 'darwin') {
         app.quit()
     }
