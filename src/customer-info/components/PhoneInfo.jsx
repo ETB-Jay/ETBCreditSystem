12fda313b63b167624513f1633b6a5ce24e9e1b9
elphoun <kale.mzhang7132@gmail.com>
2025-06-18 11:27:42 -0400

    Minor Changes

--- src/customer-info/components/PhoneInfo.jsx
+++ src/customer-info/components/PhoneInfo.jsx
@@ -20,7 +20,7 @@ function PhoneInfo() {
     return (
         <div className="flex flex-row items-center justify-start h-2/3 select-none">
             <PhoneIcon className="mr-1" sx={{ color: "white", fontSize: "max(2vw, 20px)" }} />
-            <div className="text-white font-semibold text-[0.9rem] md:text-[1rem] lg:text-[1.1rem] whitespace-nowrap max-w-8 md:max-w-20 lg:max-w-none overflow-x-scroll no-scroll">
+            <div className="text-white font-semibold text-[0.9rem] md:text-[1rem] lg:text-[1.1rem] whitespace-nowrap max-w-8 md:max-w-35 lg:max-w-none overflow-x-scroll no-scroll">
                 {formatPhoneNumber(customer.phone)}
             </div>
         </div>
